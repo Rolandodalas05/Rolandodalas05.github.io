2679723ac3de55170de85a134bdafdb30902e99f
Rolandodalas05 <dalasrolando657@gmail.com>
2024-05-25 16:28:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const btnLike1 = document.gotElementById("btnLike1")
-const countLikes1 = ducument.getElementById("countLikes1")
-const btnLike2 = document.gotElementById("btnLike2")
-const countLikes2 = ducument.getElementById("countLikes2")
+const btnLike1 = document.getElementById("btnLike1")
+const countLikes1 = document.getElementById("countLikes1")
+const btnLike2 = document.getElementById("btnLike2")
+const countLikes2 = document.getElementById("countLikes2")
+
 function clickLike1(){
-  let totalLikes1 = parseInt(countLikes1.value) +1
-  countLikes.textContent = totalLikes1.toString()
-
-}
-
+    let totalLikes1 = parseInt(countLikes1.value) + 1
+    countLikes1.textContent = totalLikes1.toString()
+  }
 function clickLike2(){
-  let totalLikes2 = parseInt(countLikes2.value) +1
-  countLikes.textContent = totalLikes2.toString()
+    let totalLikes2 = parseInt(countLikes2.value) + 1
+    countLikes2.textContent = totalLikes2.toString()
 }
-btnLikes1.addEventListener("click", clickLikes1)
-  btnLikes2.addEventListener("click", clickLikes2)
-
+btnLike1.addEventListener("click",clickLike1)
+btnLike2.addEventListener("click",clickLike2)
